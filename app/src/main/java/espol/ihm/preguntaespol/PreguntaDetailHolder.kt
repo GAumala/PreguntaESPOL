4b57e2a62a778f1d84bd4b4b1af6882551607eab
Fernando Campana <fercamp09@gmail.com>
2016-09-04 21:01:16 -0500

    Arreglos en interfaz
    Se visualiza el número de respuestas en el activity de pregunta_detail
    
    Agregue la clase usuario, anuncio, recompensa.
    
    Y agregue unos usuarios en Crear Pregunta, los usuarios se asocian con las respuestas y preguntas, agregandolas a una lista.

--- app/src/main/java/espol/ihm/preguntaespol/PreguntaDetailHolder.kt
+++ app/src/main/java/espol/ihm/preguntaespol/PreguntaDetailHolder.kt
@@ -1,19 +1,26 @@
 package espol.ihm.preguntaespol
 
 import android.view.View
+import android.widget.TextView
 
 /**
  * Created by gesuwall on 9/3/16.
  */
 
-class PreguntaDetailHolder(view: View): ScoreItemHolder(view, null) {
+class PreguntaDetailHolder(view: View, answersCount: Int): ScoreItemHolder(view, null) {
     val answerBtn: View
+    val countTextView: TextView
 
     init {
+
         answerBtn = view.findViewById(R.id.respuestas_new)
+        countTextView = view.findViewById(R.id.respuestas_title) as TextView
+        val texto = if (answersCount == 1) "Respuesta" else "Respuestas"
+        countTextView.text = answersCount.toString() + " " + texto
         answerBtn.setOnClickListener({
             val ctx = view.context as PreguntaDetailActivity
             ctx.answer()
         })
     }
+
 }
